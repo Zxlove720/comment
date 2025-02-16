663e1e8f0661b940c3e0f4c63861ec5ff7fbf9fd
wzb <2482924813@qq.com>
2025-02-16 11:38:11 +0800

    星评汇：登录完善

---
+++ comment-server/src/main/java/com/comment/interceptor/RefreshTokenInterceptor.java
@@ -0,0 +1,24 @@
+package com.comment.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class RefreshTokenInterceptor implements HandlerInterceptor {
+
+    private StringRedisTemplate stringRedisTemplate;
+
+    public RefreshTokenInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object hanlder) throws Exception {
+
+
+        return true;
+    }
+
+
+}
