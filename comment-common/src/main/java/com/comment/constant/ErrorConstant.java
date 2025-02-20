bfea9f87301539d72576f206c216921d9382e9f6
wzb <2482924813@qq.com>
2025-02-20 17:50:27 +0800

    星评汇：商户缓存

--- comment-common/src/main/java/com/comment/constant/ErrorConstant.java
+++ comment-common/src/main/java/com/comment/constant/ErrorConstant.java
@@ -8,4 +8,6 @@ public class ErrorConstant {
     public static final String PHONE_NUMBER_ERROR = "手机号格式错误";
 
     public static final String CODE_ERROR = "验证码错误";
+
+    public static final String SHOP_NOT_FOUND = "商户未找到";
 }
--- comment-server/src/main/java/com/comment/service/impl/ShopServiceImpl.java
+++ comment-server/src/main/java/com/comment/service/impl/ShopServiceImpl.java
@@ -8,6 +8,8 @@ import com.comment.mapper.ShopMapper;
 import com.comment.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -24,18 +26,27 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private HttpServletResponse httpServletResponse;
 
 
     @Override
     public Result queryShopById(Long id) {
         // 从缓存中查询商户
         String shopJson = stringRedisTemplate.opsForValue().get(ShopConstant.SHOP_CACHE_KEY + id);
-        Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-        if (shop != null) {
+        Shop shopCache = JSONUtil.toBean(shopJson, Shop.class);
+        if (shopCache != null) {
             // 命中缓存，直接返回
-            return Result.ok(shop);
+            return Result.ok(shopCache);
         }
         // 缓存未命中，从数据库中查询
+        Shop shop = query().eq("id", id).one();
+        if (shop == null) {
+            // 如果从数据库中查询失败，返回错误信息
+            httpServletResponse.setStatus(404);
+            return Result.fail("商户未找到");
+        }
+        // 查询成功，则将其加入缓存
 
 
 
