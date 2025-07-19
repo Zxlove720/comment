e180f3b40dd594652502169bff8ecaf48a50c3cb
wzb <2482924813@qq.com>
2025-07-19 10:39:53 +0800

    星评汇：全局唯一id生成器

---
+++ comment-common/src/main/java/com/comment/constant/GlobalIDConstant.java
@@ -0,0 +1,6 @@
+package com.comment.constant;
+
+public class GlobalIDConstant {
+
+    public static final String GLOBAL_ID_KEY = "comment:global:id:";
+}
--- comment-util/src/main/java/com/comment/utils/id/GlobalIDCreator.java
+++ comment-util/src/main/java/com/comment/utils/id/GlobalIDCreator.java
@@ -1,6 +1,7 @@
 package com.comment.utils.id;
 
 
+import com.comment.constant.GlobalIDConstant;
 import jakarta.annotation.Resource;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
@@ -15,36 +16,35 @@ import java.time.format.DateTimeFormatter;
 @Component
 public class GlobalIDCreator {
 
-    // 开始时间戳
-    private static final long BEGIN_TIMESTAMP = 1097712000L;
-
-    // 序列号的位数
-    private static final int COUNT_BITS = 32;
+    // 全局唯一id起始时间戳
+    private final Long BEGIN_STAMP = 1097712000L;
+    // 序列号位数
+    private final Integer COUNT_BITS = 32;
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     /**
-     * 全局唯一ID生成器
-     * 通过31位时间戳 + 32位序列号 + 1符号位拼接成的long类型的全局唯一ID
-     * @param keyPrefix 键前缀
-     * @return 全局唯一ID
+     * 获取全局唯一id
+     *
+     * @param suffix 具体业务后缀
+     * @return long 全局唯一id
      */
-    public long nextId(String keyPrefix) {
-        // 1.生成时间戳
-        LocalDateTime now = LocalDateTime.now();
-        // 获取现在时间的时间戳
-        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
-        // 通过现在时间的时间戳和开始时间戳的差生成时间戳
-        long timeStamp = nowSecond - BEGIN_TIMESTAMP;
-
+    public long getGlobalID(String suffix) {
+        // 1.获取时间戳
+        long nowTime = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        long timeStamp = nowTime - BEGIN_STAMP;
         // 2.生成序列号
-        // 获取当前日期，精确到天，每一天的键不同
-        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
-        // 序列号自增
-        long number = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 2.1获取当前的日期，以每一天进行分隔
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        Long count = stringRedisTemplate.opsForValue().increment(GlobalIDConstant.GLOBAL_ID_KEY + suffix + ":" + date);
+        return timeStamp << COUNT_BITS | count;
+    }
 
-        // 3.时间戳和序列号进行拼接，成为全局id返回
-        return timeStamp << COUNT_BITS | number;
+    /**
+     * 获取起始时间戳
+     */
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.of(2004, 10, 14, 0, 0).toEpochSecond(ZoneOffset.UTC));
     }
 }
