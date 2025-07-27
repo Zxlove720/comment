21765cddc987cb7d229454fd5e3e1e1fe9b93593
wzb <2482924813@qq.com>
2025-07-27 11:29:24 +0800

    星评汇：互斥锁

--- comment-util/src/main/java/com/comment/utils/lock/LockUtil.java
+++ comment-util/src/main/java/com/comment/utils/lock/LockUtil.java
@@ -1,5 +1,6 @@
 package com.comment.utils.lock;
 
+import cn.hutool.core.lang.UUID;
 import com.comment.constant.LockConstant;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -14,6 +15,8 @@ public class LockUtil {
 
     private final String name;
 
+    private static final String THREAD_PREFIX = UUID.randomUUID().toString(true);
+
     public LockUtil(StringRedisTemplate stringRedisTemplate, String name) {
         this.stringRedisTemplate = stringRedisTemplate;
         this.name = name;
@@ -27,10 +30,10 @@ public class LockUtil {
      */
     public boolean tryLock(long timeOut) {
         // 1.获取线程id，存入Redis
-        long id = Thread.currentThread().getId();
+        String threadId = THREAD_PREFIX + "-" + Thread.currentThread().getId();
         // 2.尝试获取锁
         String key = LockConstant.LOCK_PREFIX + name;
-        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(key, id + "", timeOut, TimeUnit.SECONDS);
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent(key, threadId, timeOut, TimeUnit.SECONDS);
         // 3.判断获取锁是否成功
         return Boolean.TRUE.equals(lock);
     }
@@ -40,14 +43,14 @@ public class LockUtil {
      */
     public void unlock() {
         // 1.获取线程id，确保每个线程只能释放自己的锁
-        long id = Thread.currentThread().getId();
+        String threadId = THREAD_PREFIX + "-" + Thread.currentThread().getId();
         // 2.判断该线程是否释放的是自己的锁
         String key = LockConstant.LOCK_PREFIX + name;
-        String threadId = stringRedisTemplate.opsForValue().get(key);
-        if (threadId == null) {
+        String value = stringRedisTemplate.opsForValue().get(key);
+        if (value == null) {
             return;
         }
-        if (!threadId.equals(id + "")) {
+        if (!threadId.equals(value)) {
             // 2.1当前线程id不等于Redis中的线程id，代表释放的不是同一把锁，直接返回
             return;
         }
